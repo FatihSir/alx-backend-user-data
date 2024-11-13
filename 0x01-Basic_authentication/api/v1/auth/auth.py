edc8f3d0518fe5f3d37900becf5febadc07314ef
FatihSir <m.saeed@ogr.iu.edu.tr>
2024-11-14 01:20:59 +0300

    fixes

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,6 +7,7 @@ from typing import List, TypeVar
 
 
 class Auth:
+    """Auth Class"""
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ A method checks if authentication is required"""
         return False
